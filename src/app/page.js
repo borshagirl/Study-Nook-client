8c8824edd4a411fe40f9df2d13751ac2174c245c
borshagirl <borshasabikunbashar@gmail.com>
2026-05-18 15:19:43 +0600

    added banner section in homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
+import Banner from "@/components/home/Banner";
 
 export default function Home() {
   return (
     <div>
-        Hello World
+        <Banner></Banner>
     </div>
   );
 }
--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,11 +1,49 @@
-
-
+import { Button, Card } from "@heroui/react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Banner = () => {
+
     return (
-        <div>
-            Banner
-        </div>
+        <section className="bg-[#0F172A] text-white px-6 py-20">
+            <div className="max-w-7xl mx-auto grid md:grid-cols-2 gap-10 items-center">
+
+                <div>
+                    <h1 className="text-5xl font-bold leading-tight">
+                        Find Your Perfect
+                        Study Room
+                    </h1>
+                    <p className="text-gray-300 mt-5">
+                        Browse and book quiet, private study rooms
+                        in your library. List your own room and earn.
+                    </p>
+                    <div className="flex gap-4 mt-8">
+
+                        <Link href="/rooms">
+                            <Button color="primary">
+                                Explore Rooms
+                            </Button>
+                        </Link>
+
+                        <Link href="/add-room">
+                            <Button variant="bordered">
+                                Add Room
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+
+                <Card className="bg-[#111827] p-4">
+                    <Image
+                        src="https://images.unsplash.com/photo-1521587760476-6c12a4b040da?q=80&w=1200"
+                        alt="Study Room"
+                        width={700}
+                        height={400}
+                        className="rounded-xl h-90 w-full object-cover"
+                    />
+                </Card>
+            </div>
+        </section>
     );
 };
 
