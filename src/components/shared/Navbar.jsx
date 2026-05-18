bd9ccd137b9cd972695d0c3c3b1c16c3ca6a859f
borshagirl <borshasabikunbashar@gmail.com>
2026-05-18 14:35:04 +0600

    Added Navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,10 +1,60 @@
+"use client";
 
+import { Button } from "@heroui/react";
+import Link from "next/link";
 
 const Navbar = () => {
+
+    const user = null;
+
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="bg-[#0F172A] text-white px-6 py-4 flex justify-between items-center">
+
+            {/* Brand */}
+            <Link href="/" className="text-xl font-bold text-indigo-400">
+                StudyNook
+            </Link>
+
+            {/* Links */}
+            <div className="flex gap-4">
+                <Link href="/">Home</Link>
+                <Link href="/rooms">Rooms</Link>
+
+                {user && (
+                    <>
+                        <Link href="/add-room">Add Room</Link>
+                        <Link href="/my-listings">My Listings</Link>
+                        <Link href="/my-bookings">My Bookings</Link>
+                    </>
+                )}
+            </div>
+
+            {/* Auth buttons */}
+            <div className="flex gap-3">
+
+                {user ? (
+                    <Button color="primary">
+                        Profile
+                    </Button>
+                ) : (
+                    <>
+                      <Link href={"/login"}>
+                        <Button as={Link} href="/login" variant="bordered">
+                            Login
+                        </Button>
+                      </Link>
+
+                      <Link href={"/register"}>
+                        <Button as={Link} href="/register" color="primary">
+                            Register
+                        </Button>
+                      </Link>
+                    </>
+                )}
+
+            </div>
+
+        </nav>
     );
 };
 
